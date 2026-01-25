68c0547cbe37361ea42531801a727c59b8812ffb
vini <luisvusa@gmail.com>
2026-01-25 09:15:15 -0300

    Add files via upload

---
+++ macbookpro/Program.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Diagnostics;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using CS2Cheat.Data.Game;
+using CS2Cheat.Features;
+using CS2Cheat.Graphics;
+using CS2Cheat.Utils;
+using static CS2Cheat.Core.User32;
+using Application = System.Windows.Application;
+
+namespace CS2Cheat;
+
+public class Program :
+    Application,
+    IDisposable
+{
+    [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+    public static extern uint TimeBeginPeriod(uint uMilliseconds);
+
+    [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+    public static extern uint TimeEndPeriod(uint uMilliseconds);
+
+    private Program()
+    {
+        OptimizePerformance();
+
+        Offsets.UpdateOffsets();
+        Startup += (_, _) => InitializeComponent();
+        Exit += (_, _) => Dispose();
+    }
+
+    private GameProcess GameProcess { get; set; } = null!;
+    private GameData GameData { get; set; } = null!;
+    private WindowOverlay WindowOverlay { get; set; } = null!;
+    private Graphics.Graphics Graphics { get; set; } = null!;
+    private BombTimer BombTimer { get; set; } = null!;
+
+    private static void OptimizePerformance()
+    {
+        // Força o relógio do Windows para 1ms
+        TimeBeginPeriod(1);
+
+        // CORREÇÃO DO ERRO: Usando o caminho completo para System.Diagnostics
+        System.Diagnostics.Process.GetCurrentProcess().PriorityClass = System.Diagnostics.ProcessPriorityClass.High;
+
+        // Modo de baixa latência do Garbage Collector
+        GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
+    }
+
+    public void Dispose()
+    {
+        GameProcess?.Dispose();
+        GameData?.Dispose();
+        WindowOverlay?.Dispose();
+        Graphics?.Dispose();
+        BombTimer?.Dispose();
+
+        TimeEndPeriod(1);
+    }
+
+    public static void Main()
+    {
+        new Program().Run();
+    }
+
+    private void InitializeComponent()
+    {
+        var features = ConfigManager.Load();
+        GameProcess = new GameProcess();
+        GameProcess.Start();
+
+        GameData = new GameData(GameProcess);
+        GameData.Start();
+
+        WindowOverlay = new WindowOverlay(GameProcess);
+        WindowOverlay.Start();
+
+        Graphics = new Graphics.Graphics(GameProcess, GameData, WindowOverlay);
+        Graphics.Start();
+
+        BombTimer = new BombTimer(Graphics);
+        if (features.BombTimer) BombTimer.Start();
+
+        if (WindowOverlay?.Window?.Handle != IntPtr.Zero)
+        {
+            SetWindowDisplayAffinity(WindowOverlay.Window.Handle, 0x00000011);
+        }
+    }
+}
