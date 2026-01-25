991f80a01cfb108802c8b392175c31fa5209cf25
vini <luisvusa@gmail.com>
2026-01-25 11:00:42 -0300

    Add files via upload

---
+++ macbookpro/Features/ESP Flags.cs
@@ -0,0 +1,56 @@
+﻿using CS2Cheat.Data.Entity;
+using SharpDX;
+using SharpDX.Direct3D9;
+using Color = SharpDX.Color;
+using Font = SharpDX.Direct3D9.Font;
+
+namespace CS2Cheat.Features;
+
+public class EspFlags
+{
+    public static void Draw(CS2Cheat.Graphics.Graphics graphics, Entity entity, Vector2 topLeft, Vector2 bottomRight)
+    {
+        var font = graphics.Gamesense;
+        if (font == null) return;
+
+        float xOffset = bottomRight.X + 4f;
+        float yOffset = topLeft.Y;
+        float spacing = 9f;
+
+        // money
+        if (entity.Money > 0)
+            DrawFlag(font, $"${entity.Money}", xOffset, ref yOffset, spacing, Color.LightGreen);
+
+        // hk
+        if (entity.ArmorValue > 0)
+            DrawFlag(font, entity.HasHelmet ? "HK" : "K", xOffset, ref yOffset, spacing, Color.White);
+
+        // zoom
+        if (entity.IsScoped)
+            DrawFlag(font, "ZOOM", xOffset, ref yOffset, spacing, Color.Cyan);
+
+    }
+
+    private static void DrawFlag(Font font, string text, float x, ref float y, float spacing, Color color)
+    {
+        var black = Color.Black;
+
+        
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                
+                if (i == 0 && j == 0) continue;
+
+                font.DrawText(null, text, (int)x + i, (int)y + j, black);
+            }
+        }
+
+        
+        font.DrawText(null, text, (int)x, (int)y, color);
+
+        
+        y += spacing;
+    }
+}
---
+++ macbookpro/Graphics/WindowOverlay.cs
@@ -0,0 +1,95 @@
+using System.Windows.Threading;
+using CS2Cheat.Data.Game;
+using CS2Cheat.Utils;
+using GameOverlay.Windows;
+using SharpDX;
+using static System.Windows.Application;
+using Color = SharpDX.Color;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace CS2Cheat.Graphics;
+
+public class WindowOverlay : ThreadedServiceBase
+{
+    public WindowOverlay(GameProcess gameProcess)
+    {
+        GameProcess = gameProcess;
+
+        Window = new OverlayWindow
+        {
+            Title = "Overlay",
+            IsTopmost = true,
+            IsVisible = true,
+            X = -32000,
+            Y = -32000,
+            Width = 16,
+            Height = 16
+        };
+
+        Window.Create();
+    }
+
+    protected override string ThreadName => nameof(WindowOverlay);
+
+    private GameProcess GameProcess { get; set; }
+
+    public OverlayWindow Window { get; private set; }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+
+        Window.Dispose();
+        Window = default;
+
+        GameProcess = default;
+    }
+
+    protected override void FrameAction()
+    {
+        Update(GameProcess.WindowRectangleClient);
+    }
+
+    private DateTime _nextCheck = DateTime.MinValue;
+
+    private void Update(Rectangle windowRectangle)
+    {
+        
+        if (DateTime.Now < _nextCheck) return;
+        _nextCheck = DateTime.Now.AddMilliseconds(500);
+
+        
+        if (Window.X == windowRectangle.Location.X &&
+            Window.Y == windowRectangle.Location.Y &&
+            Window.Width == windowRectangle.Size.Width &&
+            Window.Height == windowRectangle.Size.Height)
+            return;
+
+        
+        Current.Dispatcher.BeginInvoke(() =>
+        {
+            if (windowRectangle is { Width: > 0, Height: > 0 })
+            {
+                Window.X = windowRectangle.Location.X;
+                Window.Y = windowRectangle.Location.Y;
+                Window.Width = windowRectangle.Size.Width;
+                Window.Height = windowRectangle.Size.Height;
+            }
+        }, DispatcherPriority.Render); 
+    }
+
+    public static void Draw(GameProcess gameProcess, Graphics graphics)
+    {
+        // window border
+        graphics.DrawLine(Color.DarkGray,
+            new Vector2(0, 0),
+            new Vector2(gameProcess.WindowRectangleClient.Width - 1, 0),
+            new Vector2(gameProcess.WindowRectangleClient.Width - 1, 0),
+            new Vector2(gameProcess.WindowRectangleClient.Width - 1, gameProcess.WindowRectangleClient.Height - 1),
+            new Vector2(gameProcess.WindowRectangleClient.Width - 1, gameProcess.WindowRectangleClient.Height - 1),
+            new Vector2(0, gameProcess.WindowRectangleClient.Height - 1),
+            new Vector2(0, gameProcess.WindowRectangleClient.Height - 1),
+            new Vector2(0, 0)
+        );
+    }
+}
